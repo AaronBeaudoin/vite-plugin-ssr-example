061767d34d209d286a645f7cd4c2394743d2d3f3
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-07-25 13:37:33 -0500

    Wording tweaks

--- pages/index.page.vue
+++ pages/index.page.vue
@@ -7,6 +7,7 @@ import InlineCode from "/components/InlineCode.vue";
 </script>
 
 <template>
-  <p>Welcome to a small sample project showcasing of some of <InlineCode>vite-plugin-ssr</InlineCode>'s rendering modes. Press any of them above to see it in action.</p>
+  <p>Welcome to a small example project showcasing of some of <InlineCode>vite-plugin-ssr</InlineCode>'s rendering modes. Press any of them above to see it in action.</p>
+  <p>You can also explore this example to learn about rendering modes in general.</p>
 </template>
 
--- pages/ssr.page.vue
+++ pages/ssr.page.vue
@@ -8,5 +8,5 @@ import InlineCode from "/components/InlineCode.vue";
 </script>
 
 <template>
-  <p>This page is rendered on the <b>server-side</b>, then <a href="https://vite-plugin-ssr.com/hydration">hydrated</a> (rendered <i>again</i> on the <b>client-side</b>). This technique is the default behavior for <InlineCode>vite-plugin-ssr</InlineCode>, commonly referred to as <a href="https://vite-plugin-ssr.com/render-modes#ssr"><b>server-side rendering</b> (SSR)</a>.</p>
+  <p>This page is rendered on the <b>server-side</b>, then rendered <i>again</i> on the <b>client-side</b> during <a href="https://vite-plugin-ssr.com/hydration">hydration</a>. This technique is the default behavior for <InlineCode>vite-plugin-ssr</InlineCode>, commonly referred to as <a href="https://vite-plugin-ssr.com/render-modes#ssr"><b>server-side rendering</b> (SSR)</a>.</p>
 </template>
