3a86c574a7588c5898ce160d704816ac3a456873
Alberto Gualis <agualis@gmail.com>
2023-02-13 16:31:03 +0100

    Add wrangler setup

--- pages/index.page.vue
+++ pages/index.page.vue
@@ -1,13 +1,8 @@
-<script lang="ts">
-export const title = "Example";
-</script>
-
 <script setup lang="ts">
-import InlineCode from "/components/InlineCode.vue";
+  const title = "Example";
 </script>
 
 <template>
-  <p>Welcome to a small example project showcasing of some of <InlineCode>vite-plugin-ssr</InlineCode>'s rendering modes. Press any one of them above to see it in action.</p>
-  <p>You can also explore this example to learn about rendering modes in general.</p>
+  INDEX
 </template>
 
